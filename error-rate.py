af4d1972e3e12f31affe98883b51bce0d169a2a0
Feynman Liang <feynman.liang@gmail.com>
2016-02-04 20:05:12 +0000

    Match errror-rate.py to HResults

--- error-rate.py
+++ error-rate.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-INSWORD = -8.0
+from collections import defaultdict
+INSSCORE = 7
+SUBSCORE = 10
 
 def main(ref_mlf, other_mlf):
     with open(ref_mlf) as ref:
@@ -9,13 +11,17 @@ def main(ref_mlf, other_mlf):
 def error_rate(ref, other):
     ref_mlf = parse_mlf(ref)
     other_mlf = parse_mlf(other)
+    num_incorr = 0
     num_err = 0
     num_words = 0
     for f1, f2 in zip(ref_mlf, other_mlf):
-        (n_err, n_w) = error_best_align(f1['transcript'], f2['transcript'])
-        num_err += n_err
-        num_words += n_w
-    return num_err / num_words
+        stats = error_best_align(f1['transcript'], f2['transcript'])
+        num_incorr += stats['NUM_DEL'] + stats['NUM_SUB']
+        num_err += stats['NUM_INS'] + stats['NUM_DEL'] + stats['NUM_SUB']
+        num_words += stats['N']
+    correct = 1. - num_incorr / num_words
+    accuracy = 1. - num_err / num_words
+    return (correct, accuracy)
 
 def compare_transcripts(ref, other):
     pass
@@ -25,8 +31,8 @@ def error_best_align(ref, other):
     Computes the number of errors in the best alignment between `ref` and `other`.
     """
     (A, B) = DP(ref, other)
-    path = backtrack(A, B)
-    return (A[len(ref)][len(other)], len(B))
+    (_, stats) = backtrack(A, B)
+    return stats
 
 def DP(ref, other):
     """
@@ -44,17 +50,17 @@ def DP(ref, other):
     # base case
     B[0][0] = ('START', '')
     for i in range(1,len(ref)+1):
-        A[i][0] = i
+        A[i][0] = i*INSSCORE
         B[i][0] = ('INS_REF', ref[i-1]['word'])
     for j in range(1,len(other)+1):
-        A[0][j] = j
+        A[0][j] = j*INSSCORE
         B[0][j] = ('INS_OTHER', other[j-1]['word'])
     for i in range(1,len(ref)+1):
         for j in range(1,len(other)+1):
             actions = [
-                    (('INS_REF', (ref[i-1]['word'], '!NULL')), A[i-1][j]+1), # insertion in ref
-                    (('INS_OTHER', ('!NULL', other[j-1]['word'])), A[i][j-1]+1), # insertion in other
-                    (('SUB', (ref[i-1]['word'], other[j-1]['word'])), A[i-1][j-1]+1) # substitution
+                    (('INS_REF', (ref[i-1]['word'], '!NULL')), A[i-1][j]+INSSCORE), # insertion in ref
+                    (('INS_OTHER', ('!NULL', other[j-1]['word'])), A[i][j-1]+INSSCORE), # insertion in other
+                    (('SUB', (ref[i-1]['word'], other[j-1]['word'])), A[i-1][j-1]+SUBSCORE) # substitution
                     ]
             if ref[i-1]['word'] == other[j-1]['word']:
                 actions.append((('MATCH', ref[i-1]['word']), A[i-1][j-1]))
@@ -65,23 +71,35 @@ def DP(ref, other):
 
 def backtrack(A, B):
     """
-    Backtracks DP actions to return alignment.
+    Backtracks DP actions to return alignment and result statistics.
     """
+    stats = defaultdict(int)
+
     path = []
     i = len(B)-1
     j = len(B[0])-1
     while B[i][j][0] != 'START':
         path.append(B[i][j][1])
         if B[i][j][0] == 'INS_REF':
+            stats['N'] += 1
+            stats['NUM_DEL'] += 1
             i -= 1
         elif B[i][j][0] == 'INS_OTHER':
+            stats['NUM_INS'] += 1
             j -= 1
-        elif B[i][j][0] == 'MATCH' or B[i][j][0] == 'SUB':
+        elif B[i][j][0] == 'SUB':
+            stats['N'] += 1
+            stats['NUM_SUB'] += 1
+            i -= 1
+            j -= 1
+        elif B[i][j][0] == 'MATCH':
+            stats['N'] += 1
+            stats['H'] += 1
             i -= 1
             j -= 1
         else:
             raise Exception("Undefined action '{0}' in backtrack()".format(B[i][j]))
-    return list(reversed(path))
+    return (list(reversed(path)), stats)
 
 
 def parse_mlf(mlf):
